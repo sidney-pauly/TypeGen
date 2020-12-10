1e45c6e6b9853575f1f4104a1d52edf3c44c8033
Sidney Pauly <sidney.pauly@venios.de>
2020-12-10 05:57:01 +0100

    - Fixed generation for interfaces with ignored bases

---
+++ src/TypeGen/TypeGen.AcceptanceTest/GeneratorTestingUtils/MinimalTypeSpec.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TypeGen.Core.SpecGeneration;
+
+namespace TypeGen.AcceptanceTest.GeneratorTestingUtils
+{
+    class MinimalTypesSpec : GenerationSpec
+    {
+
+        Type MainType { get; set; }
+        Type[] OtherTypes { get; set; }
+
+        public MinimalTypesSpec(Type mainType, Type[] otherTypes)
+        {
+            MainType = mainType;
+            OtherTypes = otherTypes;
+        }
+        public override void OnBeforeGeneration(OnBeforeGenerationArgs args)
+        {
+            if (MainType.IsInterface)
+                AddInterface(MainType);
+            else
+                AddClass(MainType);
+
+            foreach (var type in OtherTypes)
+                if (type.IsInterface)
+                    AddInterface(type);
+                else
+                    AddClass(type);
+
+        }
+    }
+}
